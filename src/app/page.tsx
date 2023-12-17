4da264d0518fdc7538385c0ad74607f82a83119b
louis2704 <113580709+louis2704@users.noreply.github.com>
2023-12-17 16:21:03 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import { ConnectButton } from '../components/ConnectButton'
 import { Connected } from '../components/Connected'
 import { Token } from '../components/Token'
 import { SwapValue } from '../components/SwapValue'
+import '../../global.css'
+import { Swapper } from '../components/Swapper'
+
 export function Page() {
   return (
     <>
-    <div className='bg-blue-900'>
+    <div className='bg-blue-300 font-family'>
       <h1 className='font-[20px] font-bold text-center'>Td Swap de Louis Stoven</h1>
       <div className="flex justify-center items-center mt-[50px]">
         <div className="display-flex">
@@ -14,19 +17,20 @@ export function Page() {
         
       </div>
       <Connected>
-      <hr />
-        <h1 className="text-center font-[7px]">
+      <div className="barrehori h-[10px] w-[10px]"/>
+        <h1 className="text-center font-family font-[7px]">
           Swap ETH / SC1 : 
         </h1>
-        <h2 className="text-center font-[5px]">
+        <h2 className="text-center font-family font-[5px]">
           données via chainlink oracle
         </h2>
         <Token />
         <br />
-
-        <h2 className="text-center font-[7px]">Swap : </h2>
+        <h2 className="text-center font-family font-[7px]">Swap : </h2>
         <SwapValue/>
-        <hr />      </Connected>
+        <h1 className="barrehori h-[10px] w-[10px] pb-30"/>
+        <Swapper/>
+      </Connected>
         </div>
     </>
   )
