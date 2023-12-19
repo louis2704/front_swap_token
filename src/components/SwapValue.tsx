4170bd189452cdd5517872dc49bd4c62949c6d0f
louis2704 <113580709+louis2704@users.noreply.github.com>
2023-12-19 13:48:17 +0100

    Update SwapValue.tsx

--- src/components/SwapValue.tsx
+++ src/components/SwapValue.tsx
@@ -1,17 +1,19 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const SwapValue = () => {
+export function SwapValue() {
   return (
+    <div>
       <div>
         <SwapPrice />
       </div>
+    </div>
   );
-};
+}
 
 const SwapPrice = () => {
-  const swapPair: string = '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e';
+  const swapPair = '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e';
   const [price, setPrice] = useState<number | null>(null);
 
   useEffect(() => {
@@ -19,43 +21,45 @@ const SwapPrice = () => {
       try {
         const GET_SWAP_DATA = {
           query: `
-            query MyQuery(swapPair: String!) {
-              liquidityPositions(
-                first: 3
-                where: { pair_: { id: swapPair } }
-                orderBy: liquidityTokenBalance
-                orderDirection: desc
-              ) {
-                user {
+            query GetSwapData($pairId: ID!) {
+              pair(id: $pairId) {
+                id
+                token0 {
                   id
+                  name
                 }
-                liquidityTokenBalance
+                token1 {
+                  id
+                  name
+                }
+                reserve0
+                reserve1
               }
-              pair(id: swapPair) {
-                reserveUSD
-                totalSupply
-                name
+              swaps(first: 1, orderBy: timestamp, orderDirection: desc) {
+                amount0In
+                amount0Out
+                amount1In
+                amount1Out
+                timestamp
               }
             }
           `,
-          variables: { swapPair },
+          variables: { pairId: swapPair },
         };
 
-        const { data } = await axios.post('https://api.thegraph.com/subgraphs/name/sushiswap/exchange', GET_SWAP_DATA);
+        const response = await axios.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', GET_SWAP_DATA);
 
-        if (data && data.data && data.data.pair) {
-          const lastSwap = data.data.swaps[0];
-          const totalReserve = data.data.pair.reserve0 + data.data.pair.reserve1;
+        if (response.data && response.data.data && response.data.data.pair) {
+          const lastSwap = response.data.data.swaps[0];
+          const totalReserve = response.data.data.pair.reserve0 + response.data.data.pair.reserve1;
 
-          // Utilisez une variable différente pour stocker le prix calculé
-          const calculatedPrice = (lastSwap.amount1Out / lastSwap.amount0In) * totalReserve;
-          setPrice(calculatedPrice);
+          const price = (lastSwap.amount1Out / lastSwap.amount0In) * totalReserve;
+          setPrice(price);
         } else {
           setPrice(null);
-          console.error('GraphQL Response Data Missing:', data);
         }
 
-        console.log('Fetched Price:', price); // Cela affichera probablement 'Fetched Price: null' à cause de la nature asynchrone de setState
+        console.log('Fetched Price:', price);
       } catch (error) {
         console.error('Error fetching data:', error);
         setPrice(null);
@@ -68,11 +72,11 @@ const SwapPrice = () => {
   return (
     <>
       {price !== null ? (
-        <div className="flex justify-center font-family">
+        <div className="flex justify-center">
           <div className="my-4 mx-auto lg:mx-16 lg:w-auto border border-green-900">
             <div className="flex flex-col p-2 text-green-900">
               <div>
-                <p>Liquidité totale : {new Intl.NumberFormat('en-US').format(Math.floor(price))} $</p>
+                <p>Swap Price: {new Intl.NumberFormat('en-US').format(price)} $</p>
               </div>
             </div>
           </div>
